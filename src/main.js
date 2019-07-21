8ed1577a03f751600a91c2f55c7a5a9abb92655f
Michael Chilaka <chilakamichael001@gmail.com>
2019-07-21 13:52:08 +0100

    adding Header.vue & SearchForm.vue components

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+import axios from 'axios';
+import App from './App.vue';
+
+Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
+
+Vue.prototype.$axios;
 
 new Vue({
   el: '#app',
