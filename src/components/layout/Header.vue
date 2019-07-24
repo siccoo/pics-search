198f5d827485c00412b02ec22f82534e5cc6600d
Michael Chilaka <chilakamichael001@gmail.com>
2019-07-24 01:13:03 +0100

    update vuetify.js file

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,36 +1,62 @@
 <template>
-    <section class="header">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12 justify-center">
-                    <div class="search">
-                        <input type="text" v-model="searchStr" placeholder="Search..." required>
-                        <input type="submit" value="">
-                    </div>
-                </div>
-            </div>
+  <section class="header">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 justify-center">
+          <div class="search">
+            <form method="post">
+              <input type="search" v-model="searchStr" placeholder="Search..." required />
+              <input type="submit" value />
+            </form>
+          </div>
         </div>
-    </section>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 // import Bootstrap from 'bootstrap';
 // import fontawesome from 'font-awesome';
 
-
 export default {
-    name: Header
+  name: Header
 };
 </script>
 
 <style>
 .header {
-    background-color: #0e31bb;
-    border-radius: none !important;
+  background-color: #f5f5f5;
+  border-radius: none !important;
 }
 
 .search {
-    padding: 50px;
+  padding: 50px;
+}
+
+.search input[type="search"] {
+    outline: none;
+    padding: 8px 0px 8px 8px;
+    background: none;
+    width: 100%;
+    border: none;
+    font-size: 14px;
+    color: #fff;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+
+.search input[type="submit"] {
+    border: none;
+    cursor: pointer;
+    position: absolute;
+    outline: none;
+    top: 9px;
+    background: url(./assets/search2.png)no-repeat 0px 0px;
+    width: 25px;
+    height: 25px;
+    padding: 0;
+    right: 10px;
+	font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
 }
 </style>
 
