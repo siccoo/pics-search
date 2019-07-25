a5673d903ca6da1979dd6c7d72a340cf35ee57b8
Michael Chilaka <chilakamichael001@gmail.com>
2019-07-25 14:42:28 +0100

    Update to router folder, main.js and App.vue

--- src/components/SearchPhoto.vue
+++ src/components/SearchPhoto.vue
@@ -1,5 +1,13 @@
 <template>
-    
+    <div class="container">
+        <div class="row row-grid">
+            <div class="col-md-4">
+                <div class="unsplash-card">
+                    <img class="images" :src="img-responsive">
+                </div>
+            </div>                                                                                                                                                
+        </div>
+    </div>
 </template>
 
 <script>
--- src/router/index.js
+++ src/router/index.js
@@ -3,21 +3,24 @@ import VueRouter from 'vue-router';
 import Header from './components/layout/Header';
 import SearchPhoto from './components/SearchPhoto';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-export default new VueRouter({
-  routes: [
-    {
-      path: '/',
-      name: 'Header',
-      component: Header
-    },
-    {
-      path: '/search/:name',
-      name: 'SearchPhoto',
-      props: true,
-      component: SearchPhoto
-    }
-  ],
-  mode: 'history'
-})
+const routes = [
+  {
+    path: '/',
+    name: 'Header',
+    component: Header
+  },
+  {
+    path: '/search/:name',
+    name: 'SearchPhoto',
+    props: true,
+    component: SearchPhoto
+  }
+],
+
+
+export const router = new VueRouter({
+   mode: 'history',
+   routes
+});
